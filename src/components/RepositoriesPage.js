779e27eec8330061ceb51c17b90a9940d587e39b
Miodrag Dzamic <mdzamic@itekako.com>
2020-09-12 15:54:29 +0200

    Add loader and no result message

--- src/components/RepositoriesPage.js
+++ src/components/RepositoriesPage.js
@@ -1,28 +1,31 @@
 import React, { Fragment, Component } from "react";
 import { connect } from "react-redux";
-import RepositoryCard from "./RepositoryCard";
 import Search from "./Search";
+import RepositoryCard from "./RepositoryCard";
+import Loader from "./Loader";
 import { getRepositories } from "../actions/repositoriesAction";
 import { isArrayEmpty } from "../utils/helpers";
 
 export class RepositoriesPage extends Component {
   state = {
     dataFetching: false,
+    noResults: false,
   };
 
   onSearchButtonClick = (searchTerm) => {
     const { getRepositories } = this.props;
     this.setState({ dataFetching: true });
-    getRepositories(searchTerm, 1).then(() => {
+    getRepositories(searchTerm, 1).then((data) => {
       this.setState({
         dataFetching: false,
+        noResults: data.items.length === 0,
       });
     });
   };
 
   render() {
     const { repositories } = this.props;
-    const { dataFetching } = this.state;
+    const { dataFetching, noResults } = this.state;
     return (
       <Fragment>
         <div className="title">
@@ -32,14 +35,18 @@ export class RepositoriesPage extends Component {
           <Search onSearchButtonClick={this.onSearchButtonClick} />
         </div>
         {!dataFetching && repositories && !isArrayEmpty(repositories) && (
-          <>
-            <div className="cards">
-              {repositories.map((repository) => (
-                <RepositoryCard key={repository.id} repository={repository} />
-              ))}
-            </div>
-          </>
+          <div className="cards">
+            {repositories.map((repository) => (
+              <RepositoryCard key={repository.id} repository={repository} />
+            ))}
+          </div>
         )}
+        {!dataFetching && noResults && isArrayEmpty(repositories) && (
+          <h2 className="message">
+            No repositories available for that search, please try some other!
+          </h2>
+        )}
+        {dataFetching && <Loader />}
       </Fragment>
     );
   }
